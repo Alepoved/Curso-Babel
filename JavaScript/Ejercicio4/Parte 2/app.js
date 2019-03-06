a5484e497e57e9c7ede142c59bf80bd83bdf0282
Alepoved <alepoved@ucm.es>
2019-03-06 13:28:23 +0100

    Added ejercicio4 parte2

--- JavaScript/Ejercicio4/Parte 2/app.js
+++ JavaScript/Ejercicio4/Parte 2/app.js
@@ -108,10 +108,86 @@ function generarKeyboard() {
     console.log(key.value);
 }
 let key = document.getElementById("input_key");
-    
+let numKeys = 200;
 key.addEventListener("keydown", function(event){
-        console.log(event.keyCode);
+        event.keyCode == 8 ? numKeys++ : numKeys--;
+        console.log(event.keyCode+" Quedan "+numKeys+" caracteres");
 });
+
+const arrayImages = [];
+arrayImages[0] = "rock.jpg";
+arrayImages[1] = "paper.jpg";
+arrayImages[2] = "scissors.jpg";
+
+function jugar() {
+    let option;
+    do{
+        option = prompt("Piedra(0), Papel(1),Tijera(2)");
+    }while(option<0 || option > 2);
+
+    let randomNum =  Math.floor((Math.random() * 2 ));
+
+    let imgPlayer = document.getElementById("img2");
+    imgPlayer.src = `${arrayImages[option]}`;
+
+    let imgVS = document.getElementById("vs");
+    imgVS.src = "vs.png";
+
+    let imgCPU = document.getElementById("img3");
+    imgCPU.src = `${arrayImages[randomNum]}`;
+
+    if(option == 0){
+        randomNum == 0 ? console.log("Empate") : randomNum==1 ? console.log("Has perdido :(") : console.log("Has ganado :)") ;
+    }
+    else if (option == 1){
+        randomNum == 0 ? console.log("Has ganado :)") : randomNum==1 ? console.log("Empate") : console.log("Has perdido :(") ;
+    }
+    else{
+        randomNum == 0 ? console.log("Has perdido :(") : randomNum==1 ? console.log("Has ganado :)") : console.log("Empate") ;
+    }
+}
+
+function mover(){
+    let divMove = document.getElementById("move");
+    let right = 0; let top = 0;
+    let keyMove = document.addEventListener("keydown",function(event){
+        if(event.keyCode == 39){//right
+            right +=20;
+            divMove.style.marginLeft = `${right}px`;
+        }
+        else if(event.keyCode == 37){//left
+            right -=20;
+            divMove.style.marginLeft = `${right}px`;
+        }
+        else if(event.keyCode == 38){//top
+            top -=20;
+            divMove.style.marginTop = `${top}px`;
+        }
+        else if(event.keyCode == 40){//bottom
+            top +=20;
+            divMove.style.marginTop = `${top}px`;
+        }
+       
+    });
+}
+
+function getMiddle(s)
+{
+  //Code goes here!
+  let array = [];
+  array = s.split("");
+  let res;
+  let length = array.length;
+  if(length % 2 == 0){
+    res = array[length/2-1]+""+array[length/2];
+  }
+  else{
+      console.log(length);
+    res = array[Math.floor(length/2)];
+}
+return res;
+}
+
 
 
 
